438989e38174d88e8ec646331aa7db8634b3b60a
Lorenzo Sciandra <lorenzo.sciandra.business@gmail.com>
2020-07-02 14:35:00 +0200

    fine

--- src/GUI/Login.py
+++ src/GUI/Login.py
@@ -10,11 +10,12 @@ from src.GUI import TextBox
 
 
 def login():
-    file = open("../FilePassword/pp.txt","r")
-    p = file.read()
-    s = entry.get()
-    if(len(p) > 0):
+    fileKey = open('../FilePassword/key.key', 'rb')
+    key = fileKey.read()  # The key will be type bytes
+    fileKey.close()
 
+    s = entry.get()
+    if(len(key) > 0):
         s = s.replace(" ", "")
         s = s.encode()
         salt = b'salt_'  # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
@@ -25,16 +26,14 @@ def login():
             iterations=100000,
             backend=default_backend()
         )
-        key = base64.urlsafe_b64encode(kdf.derive(s))
-        encoding = 'utf-8'
+        keyInserita = base64.urlsafe_b64encode(kdf.derive(s))
 
-        if(p == str(key, encoding)):
-            print('Uguali')
-            TextBox.modify()
+        if(key == keyInserita):
+            TextBox.modify(screen)
         else:
             messagebox.showerror('Password Errata', 'La password principale di sistema non corrisponde')
     else:
-        fileWrite = open("../FilePassword/pp.txt", "wb")
+
         string_value = simpledialog.askstring('Non hai ancora una password!', 'Che password di sistema vuoi usare? \nSceglila bene, sarà quella che dovrai usare ogni volta che accesi!',)
         string_value = string_value.replace(" ", "")
         string_value = string_value.encode()
@@ -47,9 +46,11 @@ def login():
             backend=default_backend()
         )
         key = base64.urlsafe_b64encode(kdf.derive(string_value))
-        fileWrite.write(key)
+        file = open('key.key', 'wb')
+        file.write(key)  # The key is type bytes still
+        file.close()
 
-
+global screen
 screen = Tk()
 screen.geometry("600x400")
 screen.title("Login")
@@ -60,10 +61,6 @@ entry.pack()
 Label(text = "").pack()
 b = Button(text="Login", height="2", width="30", command=login)
 b.pack()
-
-
-
-
 screen.mainloop()
 
 
--- src/GUI/TextBox.py
+++ src/GUI/TextBox.py
@@ -2,23 +2,86 @@ from tkinter import *
 from tkinter import messagebox, simpledialog
 import base64
 import os
+
+from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+from tkinter import *
 
-def save_file(self):
-    print("Save file")
 
-def modify():
-    root = Tk()
-    root.title("Password")
-    text_area = Text()
-    text_area.pack(fill=BOTH, expand=1)
 
-    main_menu = Menu()
-    root.config(menu=main_menu)
-    file_menu = Menu(main_menu)
-    main_menu.add_cascade(label="File", menu=file_menu)
-    file_menu.add_command(label="Save", command=save_file)
+def addAll(text):
+    fileTesto = "../FilePassword/AllPasswords.encrypted"
+    output_file = "../FilePassword/Decifrate.txt"
+    with open(fileTesto, 'rb') as f:
+        encrypt = f.read()
+
+    if(len(encrypt)>0):
+        fileChiave = open("../FilePassword/key.key", "rb")
+        key = fileChiave.read()  # The key will be type bytes
+        fileChiave.close()
+
+        f = Fernet(key)
+        decrypted = f.decrypt(encrypt)
+
+        with open(output_file, 'wb') as f:
+            f.write(decrypted)
+
+        fileFinale = open("../FilePassword/Decifrate.txt", "r")
+        lines = fileFinale.readlines()
+
+        for i in range(len(lines)):
+            text.insert(END, lines[i])
+
+        fileFinale.close()
+
+
+
+def save():
+    t = text.get("1.0", "end-1c")
+
+
+    if(len(t))>0:
+
+        input_file = '../FilePassword/Decifrate.txt'
+        output_file = '../FilePassword/AllPasswords.encrypted'
+
+        with open(input_file, 'w') as f:
+            f.write(t)
+
+        with open(input_file, 'rb') as f:
+            data = f.read()
+
+        fileChiave = open("../FilePassword/key.key", "rb")
+        key = fileChiave.read()  # The key will be type bytes
+        fileChiave.close()
+
+        fernet = Fernet(key)
+        encrypted = fernet.encrypt(data)
+
+        with open(output_file, 'wb') as f:
+            f.write(encrypted)
+
+        with open(input_file, 'w') as f:
+            f.write("nulla")
+
+    root.destroy()
+
+
+def modify(screen):
+    global root
+    global text
+    root = Toplevel(screen)
+    root.title("Password Editor")
+    text = Text(root)
+    addAll(text)
+    text.pack()
+    Label(root, text="").pack()
+    button = Button(root, text="Salva", command=save)
+    button.pack()
+    Label(root,text="").pack()
+    root.mainloop()
+
 
